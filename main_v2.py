87c30569753a6cf4a8ecb145827b0daa3d0edbd3
RishiteshPerikala <perikalarishi@gmail.com>
2026-04-09 23:43:02 +0530

    Converted combined text features to TF-IDF vectors and encoded labels for V2

--- main_v2.py
+++ main_v2.py
@@ -8,7 +8,6 @@ data = pd.read_csv("data/train.tsv", sep="\t", header=None, names=columns)
 
 # Show first rows
 print(data.head())
-print("\nColumns:\n", data.columns)
 
 # Fill missing values to avoid errors
 text_columns = ["statement", "subject", "speaker","speaker_job", "state", "party", "context"]
@@ -24,3 +23,20 @@ data = data[["label", "full_text"]]
 
 # Check output
 print(data.head())
+
+# Using TF-IDF & Encoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+# Convert text to numerical features
+vectorizer = TfidfVectorizer(max_features=5000)
+
+X = vectorizer.fit_transform(data["full_text"]).toarray()
+
+# Encode labels
+encoder = LabelEncoder()
+y = encoder.fit_transform(data["label"])
+
+# Check shapes
+print("Feature shape:", X.shape)
+print("Label shape:", y.shape)
