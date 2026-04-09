7ef40ac31c3609a773a8be0356e2e0dfb0075455
RishiteshPerikala <perikalarishi@gmail.com>
2026-04-10 00:01:19 +0530

    Trained V2 baseline model using combined features and evaluated accuracy

--- main_v2.py
+++ main_v2.py
@@ -40,3 +40,23 @@ y = encoder.fit_transform(data["label"])
 # Check shapes
 print("Feature shape:", X.shape)
 print("Label shape:", y.shape)
+
+# Splitting Dataset
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=57)
+
+# Train model
+model = LogisticRegression(max_iter=500)
+model.fit(X_train, y_train)     # trained model
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("V2 Accuracy:", accuracy)
